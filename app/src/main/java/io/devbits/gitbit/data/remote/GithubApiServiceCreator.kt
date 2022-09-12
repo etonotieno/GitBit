f807340384d92a9b2abd37807ebd9b72d0f0ad91
Eton Otieno <etonotieno@gmail.com>
2022-09-12 18:38:37 +0300

    Add an OkHttpClient to log network calls in DEBUG builds

--- app/src/main/java/io/devbits/gitbit/data/remote/GithubApiServiceCreator.kt
+++ app/src/main/java/io/devbits/gitbit/data/remote/GithubApiServiceCreator.kt
@@ -1,15 +1,29 @@
 package io.devbits.gitbit.data.remote
 
+import io.devbits.gitbit.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 object GithubApiServiceCreator {
 
+    private fun createOkhttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) Level.BODY else Level.NONE
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
     fun getRetrofitClient(): GithubApiService {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(createOkhttpClient())
             .build()
             .create()
     }
